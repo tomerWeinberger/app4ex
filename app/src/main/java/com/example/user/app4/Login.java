3193c90e2dc16225f2b722f6457774d4a9216718
tomer_weinberger <tomerwme@gmail.com>
2016-06-10 16:58:25 +0300

    fff

--- app/src/main/java/com/example/user/app4/Login.java
+++ app/src/main/java/com/example/user/app4/Login.java
@@ -1,29 +1,112 @@
 package com.example.user.app4;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText nameText;
+    EditText passwordText;
+    Button loginButton;
+    Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        nameText = (EditText) findViewById(R.id.input_name);
+        loginButton = (Button) findViewById(R.id.btn_login);
+        regButton = (Button) findViewById(R.id.btn_Reg);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
             }
         });
     }
 
+    private void login() {
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(Login.this,
+                R.style.AppTheme_PopupOverlay);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = nameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        String name = nameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (name.isEmpty()) {
+            nameText.setError("enter a valid name");
+            valid = false;
+        } else {
+            nameText.setError(null);
+        }
+
+        if (password.isEmpty()) {
+            passwordText.setError("enter a valid password");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
+    }
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        loginButton.setEnabled(true);
+    }
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+        finish();
+    }
 }
---
+++ app/src/main/java/com/example/user/app4/Register.java
@@ -0,0 +1,133 @@
+package com.example.user.app4;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    private EditText usernameIn;
+    private EditText emailIn;
+    private EditText passwordIn;
+    private EditText nameIn;
+    private Button signupButton;
+    private TextView loginLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        usernameIn = (EditText) findViewById(R.id.input_username);
+        passwordIn = (EditText) findViewById(R.id.input_password);
+        nameIn = (EditText) findViewById(R.id.input_namer);
+        emailIn = (EditText) findViewById(R.id.input_email);
+
+        signupButton = (Button) findViewById(R.id.btn_signup);
+        loginLink = (TextView) findViewById(R.id.link_login);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(Register.this,
+                R.style.AppTheme_PopupOverlay);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = usernameIn.getText().toString();
+        String email = emailIn.getText().toString();
+        String password = passwordIn.getText().toString();
+        String pvtName = nameIn.getText().toString();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Register failed", Toast.LENGTH_LONG).show();
+
+        signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = usernameIn.getText().toString();
+        String email = emailIn.getText().toString();
+        String password = passwordIn.getText().toString();
+        String namePvt = nameIn.getText().toString();
+
+        if (name.isEmpty()) {
+            usernameIn.setError("at least 1 characters");
+            valid = false;
+        } else {
+            usernameIn.setError(null);
+        }
+        if (namePvt.isEmpty()) {
+            nameIn.setError("at least 1 characters");
+            valid = false;
+        } else {
+            nameIn.setError(null);
+        }
+        if (email.isEmpty()) {
+            emailIn.setError("enter a valid email address");
+            valid = false;
+        } else {
+           emailIn.setError(null);
+        }
+
+        if (password.isEmpty()) {
+            passwordIn.setError("at least 1 characters");
+            valid = false;
+        } else {
+            passwordIn.setError(null);
+        }
+
+        return valid;
+    }
+}
+
+
